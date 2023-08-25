cff48c0ad13e023ac9abc64020d0efd59ae362f8
Jordan O'Leary <jdoleary@gmail.com>
2023-08-25 12:45:29 -0400

    chore: Add builds for mac and pc

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-deno compile --allow-net --allow-write --allow-read src/index.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden.exe --target x86_64-pc-windows-msvc src/index.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-x86-64 --target x86_64-apple-darwin src/index.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-aarch --target aarch64-apple-darwin src/index.ts
