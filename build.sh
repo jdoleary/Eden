ccf0683a4b25adf1cf69ab3c130e416891880af0
Jordan O'Leary <jdoleary@gmail.com>
2023-09-08 12:59:15 -0400

    fix: entrypoint of build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-deno compile --allow-net --allow-write --allow-read --output executable/eden.exe --target x86_64-pc-windows-msvc src/index.ts
-deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-x86-64 --target x86_64-apple-darwin src/index.ts
-deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-aarch --target aarch64-apple-darwin src/index.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden.exe --target x86_64-pc-windows-msvc src/main.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-x86-64 --target x86_64-apple-darwin src/main.ts
+deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-aarch --target aarch64-apple-darwin src/main.ts
 chmod +x executable/eden-mac-x86-64
 chmod +x executable/eden-mac-aarch
