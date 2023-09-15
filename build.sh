7daf95d113baad22bb0a7d90a2c51208beed3da2
Jordan O'Leary <jdoleary@gmail.com>
2023-09-15 16:33:16 -0400

    chore: Add EULA

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+cp LICENCE.txt executable/LICENCE.txt
+cp LICENCE.txt executable/LICENCE.md
 deno compile --allow-net --allow-write --allow-read --output executable/eden.exe --target x86_64-pc-windows-msvc src/main.ts
 deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-x86-64 --target x86_64-apple-darwin src/main.ts
 deno compile --allow-net --allow-write --allow-read --output executable/eden-mac-aarch --target aarch64-apple-darwin src/main.ts
