f121bc947a95bb67a2eef52df2669b5a420a440a
Jordan O'Leary <jdoleary@gmail.com>
2023-10-08 09:37:34 -0400

    fix: ./test.sh to run tests on src/main instead of src/mod.ts because src/mod.ts actually triggers main to run on a directory which creates async leaks in the test

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 # Test without updating snapshots
-MODE=test deno test --allow-all src/mod.ts -- --parseDir test/
+MODE=test deno test --allow-env src/main.ts -- --parseDir test/
 
 # Update snapshots
-# MODE=test deno test --allow-all src/mod.ts --parseDir test/ -- --update
+# MODE=test deno test --allow-env src/main.ts --parseDir test/ -- --update
 
 # Preview test dir
-# deno run --allow-all src/mod.ts --parseDir test/ --preview && start http://localhost:8000/Table_of_Contents.html
+# deno run --allow-env src/main.ts --parseDir test/ --preview && start http://localhost:8000/Table_of_Contents.html
