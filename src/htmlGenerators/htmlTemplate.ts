3b1cc48e924a2601fb2d9c15fda2e19c6eaf8352
Jordan O'Leary <jdoleary@gmail.com>
2023-10-08 09:48:39 -0400

    css: Keep made-width footer from
    being in the center of tall index pages

--- src/htmlGenerators/htmlTemplate.ts
+++ src/htmlGenerators/htmlTemplate.ts
@@ -163,7 +163,6 @@ article {
     padding:0 1em;
     background-color: #ffffff;
     width:100%;
-    min-height: 100%;
     box-sizing: border-box;
     /* So small pages will grow to full height */
     flex: 1;
